eced51cd75e3811b03eddbe12ebbb231e3ca8fd7
anum <anum301@outlook.com>
2024-08-10 21:12:10 +0100

    set up header and router

---
+++ flower-shop/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FlowerList from '../components/FlowerList/FlowerList';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to the Flower Shop!</h2>
+      <FlowerList />
+    </div>
+  );
+};
+
+export default Home;
